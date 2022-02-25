a0c134c76ab63ed2173d819ac1e1596e3c090480
Chem537 Applied Computational Chemistry <97694179+WSUChem537@users.noreply.github.com>
2022-02-25 09:17:45 -0800

    Update packmole-dat.sh

--- packmole-dat.sh
+++ packmole-dat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -e '1,2d'  Arbox-new.xyz > test.xyz
+sed -e '1,2d'  Arbox-new.xyz > test.xyz #deletes first two lines and moves the new file into a test.xyz file
 
-for n in {1..100}
+for n in {1..100}  #looping over the total number atoms/lines to do a replacement of the Ar (for argon box) into the ID number and the Group ID
 do
     sed -i $n's/Ar/'$n'	1	/' test.xyz 
 done 
